6472ec73093a0bc3ef17da5991f84bdba7d97dbf
EddyBautista <bautista.j.eddy@gmail.com>
2020-12-01 12:21:31 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _24DaysOfChristmasCodeingChallenges
+{
+    class Program
+    {
+      
+
+        private static void Main(string[] args)
+        {
+            //  Solve("Zodiac");
+            Solve("chruschtschov");
+
+        }
+    }
+}
