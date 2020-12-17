e0e97eb456de69944ac5f711dd918a1a2c99ea45
EddyBautista <bautista.j.eddy@gmail.com>
2020-12-17 16:21:13 -0600

    day 10

--- Program.cs
+++ Program.cs
@@ -332,21 +332,41 @@ namespace _24DaysOfChristmasCodeingChallenges
         // solve("CODe") = "CODE". Uppercase characters > lowecase.Change only the "e" to uppercase.
         // solve("coDE") = "code". Upper == lowercase.Change all to lowercase.
 
-        public static string Solve(string s)
+        //  public static string Solve(string s)
+        //  {
+        //      return s.Count(char.IsLower) < s.Length / 2 ? s.ToUpper() : s.ToLower();
+        //  }
+
+        // Day 10 - Persistent
+
+        // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+        // 
+        // For example:
+        // 
+        //  persistence(39) == 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+        //                       // and 4 has only one digit
+        // 
+        //  persistence(999) == 4 // because 9*9*9 = 729, 7*2*9 = 126,
+        //                        // 1*2*6 = 12, and finally 1*2 = 2
+        // 
+        //  persistence(4) == 0 // because 4 is already a one-digit number
+
+        public static int Persistence(long n)
         {
-            return s.Count(char.IsLower) < s.Length / 2 ? s.ToUpper() : s.ToLower();
+            int count = 0;
+            while (n > 9)
+            {
+                count++;
+                n = n.ToString().Select(digit => int.Parse(digit.ToString())).Aggregate((x, y) => x * y);
+            }
+            return count;
         }
 
 
 
         private static void Main(string[] args)
         {
-            Solve("coDe");
-            Solve("CODe");
-            Solve("coDE");
-
-
-
+            Persistence(39);
         }
     }
 }
